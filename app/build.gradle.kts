2b8728914f26b80e6a69681a6957268eade11114
marinhen <makravchenko@griddynamics.com>
2025-02-14 17:12:50 +0100

    Initial commit: project structure setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kapt)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.compose.compiler)
+}
+
+apply(from = "../config/gradle/build-scripts/android.gradle")
+apply(from = "../config/gradle/build-scripts/compose.gradle")
+
+
+android.defaultConfig {
+    applicationId = "com.example.trendhub"
+}
+
+dependencies {
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
+    // Hilt
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.compose)
+    kapt(libs.hilt.compiler)
+
+    // Modules
+    implementation(project(":core"))
+    implementation(project(":feature:trending"))
+}
---
+++ core/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+}
+
+apply(from = "../config/gradle/build-scripts/android.gradle")
+apply(from = "../config/gradle/build-scripts/compose.gradle")
+
+android {
+    namespace = "com.example.core"
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
